96f0cb5de47480fd9f7f30408a077300358e7725
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-16 19:08:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index{ |item,index| puts "#{index + 1}.#{item}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map{ |call| "#{call.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # cheese_or_no_cheese = array.find{ |i| i.include?(cheese_types) }
+  # if cheese_or_no_cheese
+  #   return i
+  # else
+  #   nil
+  # end
+  
+   yes = array.any? { |i| cheese_types.include?(i) }
+  if yes
+    return yes[i]
+  end
+  
+  puts nil if array.any? { |i| not cheese_types.include?(i) }
+  
 end
