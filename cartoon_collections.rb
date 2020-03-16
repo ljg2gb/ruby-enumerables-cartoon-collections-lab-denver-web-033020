54c311a00b9ed813047fe2477c140ffc5111fef7
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-16 19:12:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(array)
   
   yes = array.any? { |i| cheese_types.include?(i) }
   if yes
-    return "gouda"
+    return array & cheese_types
   end
   
   puts nil if array.any? { |i| not cheese_types.include?(i) }
